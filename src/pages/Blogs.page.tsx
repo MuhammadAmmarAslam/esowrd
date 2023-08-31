614d485b209c4d8843b79585916bbb6007352a27
Ammar <136464491+MuhammadAmmarAslam@users.noreply.github.com>
2023-08-31 12:00:07 +0500

    Update Blogs.page.tsx

--- src/pages/Blogs.page.tsx
+++ src/pages/Blogs.page.tsx
@@ -3,7 +3,7 @@ import BooksChapterComponent from "@/components/BooksChapterComponent";
 import HeaderComponent from "@/components/HeaderComponent";
 import PaginationComponent from "@/components/PaginationComponent";
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@radix-ui/react-accordion";
-import { ChevronDownIcon } from "@radix-ui/react-icons";
+import { BookmarkFilledIcon, ChevronDownIcon } from "@radix-ui/react-icons";
 
 
 
@@ -40,9 +40,9 @@ export default function BlogsPage() {
                         </h3>
                     </div>
                     <div className="lg:grid grid-cols-11 ">
-                        <div className="col-span-7  w-full p-5  text-primary-dark text-base font-normal font-roman   ">
-                            <div className="flex  gap-3">
-                                <div className="space-y-3 max-h-[calc(100vh_-_100px)] overflow-auto">
+                        <div className="col-span-7  w-full  text-primary-dark text-base font-normal font-roman lg:border-r-[10px]  ">
+                            <div className="flex ">
+                                <div className="space-y-3 max-h-[calc(100vh_-_100px)] overflow-y-auto w-full ">
                                     <p>
                                         All beginnings must begin with God. Always put I for trotoite teat tons no the has an end first thought every morning, the first aim and 4 purpose of all activity. Bein the took tree year with God, and you will end it with the glory of the New Jerusalem. At first, as in the physical creation,  and life may seem to be "without form and void." Do not be discouraged, the Spirit of God is within you,  amid the darkness, and presently His Light will shine through.
                                     </p>
@@ -83,21 +83,61 @@ export default function BlogsPage() {
                                         All beginnings must begin with God. Always put I for trotoite teat tons no the has an end first thought every morning, the first aim and 4 purpose of all activity. Bein the took tree year with God, and you will end it with the glory of the New Jerusalem. At first, as in the physical creation,  and life may seem to be "without form and void." Do not be discouraged, the Spirit of God is within you,  amid the darkness, and presently His Light will shine through.
                                     </p>
                                 </div>
-
-                                <div className="max-h-[calc(100vh_-_100px)] overflow-auto min-w-[40px] text-center space-y-4">
-                                    <img src="/icons/bookfill-icon.png" alt="" className="w-auto h-auto" />
-                                    <img src="/icons/bookfill-icon.png" alt="" className="w-auto h-auto" />
-                                    <img src="/icons/bookfill-icon.png" alt="" className="w-auto h-auto" />
-                                    <img src="/icons/bookfill-icon.png" alt="" className="w-auto h-auto" />
-                                    <img src="/icons/bookfill-icon.png" alt="" className="w-auto h-auto" />
-                                    <img src="/icons/bookfill-icon.png" alt="" className="w-auto h-auto" />
-                                    <img src="/icons/bookfill-icon.png" alt="" className="w-auto h-auto" />
-                                   
+                                <div className="flex flex-col gap-4 pt-3 px-2  overflow-y-auto max-h-[calc(100vh_-_100px)] w">
+                                    <a href="">
+                                        <img src="/icons/bookfill-icon.png " className="bookmark" />
+                                    </a>
+                                    <a href="">
+                                        <img src="/icons/bookfill-icon.png " className="bookmark" />
+                                    </a>
+                                    <a href="">
+                                        <img src="/icons/bookfill-icon.png " className="bookmark" />
+                                    </a>
+                                    <a href="">
+                                        <img src="/icons/bookfill-icon.png " className="bookmark" />
+                                    </a>
+                                    <a href="">
+                                        <img src="/icons/bookfill-icon.png " className="bookmark" />
+                                    </a>
+                                    <a href="">
+                                        <img src="/icons/bookfill-icon.png " className="bookmark" />
+                                    </a>
+                                    <a href="">
+                                        <img src="/icons/bookfill-icon.png " className="bookmark" />
+                                    </a>
+                                    <a href="">
+                                        <img src="/icons/bookfill-icon.png " className="bookmark" />
+                                    </a>
+                                    <a href="">
+                                        <img src="/icons/bookfill-icon.png " className="bookmark" />
+                                    </a>
+                                    <a href="">
+                                        <img src="/icons/bookfill-icon.png " className="bookmark" />
+                                    </a>
+                                    <a href="">
+                                        <img src="/icons/bookfill-icon.png " className="bookmark" />
+                                    </a>
+                                    <a href="">
+                                        <img src="/icons/bookfill-icon.png " className="bookmark" />
+                                    </a>
+                                    <a href="">
+                                        <img src="/icons/bookfill-icon.png " className="bookmark" />
+                                    </a>
+                                    <a href="">
+                                        <img src="/icons/bookfill-icon.png " className="bookmark" />
+                                    </a>
+                                    <a href="">
+                                        <img src="/icons/bookfill-icon.png " className="bookmark" />
+                                    </a>
+                                    <a href="">
+                                        <img src="/icons/bookfill-icon.png " className="bookmark" />
+                                    </a>
+                                  
+                                  
                                 </div>
                             </div>
-
-
                         </div>
+
                         <div className="col-span-4 bg-primary lg:block hidden relative">
                             <div className="p-5 space-y-4  overflow-auto  min-h-[calc(100vh_-_100px)] max-h-[calc(100vh_-_100px)]">
                                 <BlogsContent />
