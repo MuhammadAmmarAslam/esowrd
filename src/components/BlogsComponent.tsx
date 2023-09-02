36135ac3ec029a56245659910e176fd6260d9f8d
Ammar <136464491+MuhammadAmmarAslam@users.noreply.github.com>
2023-09-02 13:02:48 +0500

    Update BlogsComponent.tsx

--- src/components/BlogsComponent.tsx
+++ src/components/BlogsComponent.tsx
@@ -61,11 +61,11 @@ export function BlogsComponent({ heading, date, footer }: Props) {
         <Card className="bg-white w-full">
             <CardContent>
                 <div className="md:space-y-1">
-                    <div className="flex gap-1 xl:gap-2 items-center">
-                        <h1 className="font-bold text-lg md:text-2xl lg:text-sm  font-roman text-primary-dark/80 xl:text-lg" >
+                    <div className="flex gap-1  items-center">
+                        <h1 className="font-bold text-lg md:text-2xl lg:text-xs  font-roman text-primary-dark/80 xl:text-xl" >
                             {heading}
                         </h1>
-                        <p className="font-normal text-xs md:text-base font-inter lg:text-[8px] text-primary-dark xl:text-sm">
+                        <p className="font-normal text-xs md:text-base font-inter lg:text-[7px] text-primary-dark xl:text-xs">
                             {date}
                         </p>
                     </div>
